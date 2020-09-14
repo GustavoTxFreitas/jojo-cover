3e8cdb48873e2d54ff9c1bf66058f293cbd2f845
Sinayra <nayra@hotmail.co.jp>
2020-09-14 16:47:46 -0300

    Remove gatsby typescript css module

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,6 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-typescript`,
-    "gatsby-transformer-typescript-css-modules"
+    `gatsby-plugin-typescript`
   ],
 }
